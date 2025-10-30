ed806e4644b7a328b02339c8bc6592da0bffbde0
Lohos06 <122732120+Lohos06@users.noreply.github.com>
2025-10-30 11:57:18 +0100

    fix|CerveauInteractifFactorisé

--- JavaScript/cerveauInteractif.js
+++ JavaScript/cerveauInteractif.js
@@ -3,37 +3,35 @@ let LobeParietal = document.getElementById('LobeParietal');
 
 let LobeFrontalModal = document.getElementById('LobeFrontalModal');
 let LobeParietalModal = document.getElementById('LobeParietalModal');
-           
+      
+function OtherModalsInactive () {
+    LobeFrontalModal.classList.remove('active');
+    LobeFrontalModal.classList.add('inactive');
+
+    LobeParietalModal.classList.remove('active');
+    LobeParietalModal.classList.add('inactive');
+}
+function ModalActive (Modal) {
+    Modal.classList.remove('inactive');
+    Modal.classList.add('active');
+}
+
 LobeFrontal.addEventListener('click', () => {
+    
     if (LobeFrontalModal.classList.contains('active')) {
-
-        LobeFrontalModal.classList.remove('active');
-        LobeFrontalModal.classList.add('inactive');
-
+        OtherModalsInactive();
     } else {
-
-        LobeParietalModal.classList.remove('active');
-        LobeParietalModal.classList.add('inactive');
-
-        LobeFrontalModal.classList.remove('inactive');
-        LobeFrontalModal.classList.add('active');
+        OtherModalsInactive();
+        ModalActive(LobeFrontalModal);
     }
 })
 
 LobeParietal.addEventListener('click', () => {
 
     if (LobeParietalModal.classList.contains('active')) {
-
-        LobeParietalModal.classList.remove('active');
-        LobeParietalModal.classList.add('inactive');
-
-
+        OtherModalsInactive();
     } else {
-
-        LobeFrontalModal.classList.remove('active');
-        LobeFrontalModal.classList.add('inactive');
-
-        LobeParietalModal.classList.remove('inactive');
-        LobeParietalModal.classList.add('active');
+        OtherModalsInactive();
+        ModalActive(LobeParietalModal);
     }
 })
