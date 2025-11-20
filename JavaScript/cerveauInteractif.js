502fd0533389192f0fd6ba012f5edb15ee0f0c93
Lohos06 <122732120+Lohos06@users.noreply.github.com>
2025-11-20 09:26:25 +0100

    fix|mapping du cerveau et ses modales

--- JavaScript/cerveauInteractif.js
+++ JavaScript/cerveauInteractif.js
@@ -3,35 +3,31 @@ let LobeParietal = document.getElementById('LobeParietal');
 
 let LobeFrontalModal = document.getElementById('LobeFrontalModal');
 let LobeParietalModal = document.getElementById('LobeParietalModal');
-      
-function OtherModalsInactive () {
-    LobeFrontalModal.classList.remove('active');
-    LobeFrontalModal.classList.add('inactive');
 
-    LobeParietalModal.classList.remove('active');
-    LobeParietalModal.classList.add('inactive');
+let LobeAndsModals = new Map();
+LobeAndsModals.set(LobeFrontal, LobeFrontalModal);
+LobeAndsModals.set(LobeParietal, LobeParietalModal);
+      
+function ModalsInactive () {
+    LobeAndsModals.forEach((Modal) => {
+        Modal.classList.remove('active');
+        Modal.classList.add('inactive');
+    })
 }
+
 function ModalActive (Modal) {
     Modal.classList.remove('inactive');
     Modal.classList.add('active');
 }
 
-LobeFrontal.addEventListener('click', () => {
-    
-    if (LobeFrontalModal.classList.contains('active')) {
-        OtherModalsInactive();
-    } else {
-        OtherModalsInactive();
-        ModalActive(LobeFrontalModal);
-    }
-})
-
-LobeParietal.addEventListener('click', () => {
-
-    if (LobeParietalModal.classList.contains('active')) {
-        OtherModalsInactive();
-    } else {
-        OtherModalsInactive();
-        ModalActive(LobeParietalModal);
-    }
+LobeAndsModals.forEach((Modal, Lobe) => {
+    Lobe.addEventListener('click', function() {
+        if (Modal.classList.contains('active')) {
+            ModalsInactive();
+        } else {
+            ModalsInactive();
+            ModalActive(Modal);
+        }
+        console.log(Modal);
+    })
 })
